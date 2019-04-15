5bf7857be9ede61cbdd31e57b5e50d65bb152ef8
unknown <rizqiamalia1703@gmail.com>
2019-04-16 00:03:24 +0700

    Rizqi Amalia-pertemuan1

---
+++ Rizqi Amalia/riris3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    float k,l;
+    scanf("%d %d", &i, &j);
+    scanf("%f %f", &k, &l);
+
+    printf("%d %d\n", i+j, i-j);
+    printf("%.1f %.1f\n", k+l, k-l);
+    return 0;
+}
