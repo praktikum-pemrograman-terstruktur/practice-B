320e9e84c45e6e67edf646ad07f2fd88aa566807
Dinda Maulidiyah Rachman <49421130+Dindamr@users.noreply.github.com>
2019-04-22 17:09:34 +0700

    Add files via upload
    Dinda MR - Pertemuan 2

---
+++ DINDAMR/Sum Digits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+	
+    int n, jumlah;
+    scanf ("%d", &n);
+    while (n>0)
+    {
+        jumlah+=(n%10);
+        n=n/10;
+    }
+     printf ("%d\n", jumlah);
+  
+    return 0;
+}
